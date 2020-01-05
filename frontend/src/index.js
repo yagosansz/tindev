8a513205263f08a3e889d5b542b86bbaf4ce12b2
Yago Santos de Sousa <yagosansz@gmail.com>
2020-01-05 14:37:58 -0500

    Initial commit frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // Biblioteca necessaria para o React interagir com a Arvore de Elementos
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
