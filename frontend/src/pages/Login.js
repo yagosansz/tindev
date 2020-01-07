67beac132cd7a402065a164e1f31072fbcd0db4e
Yago Santos de Sousa <yagosansz@gmail.com>
2020-01-07 18:32:28 -0500

    Added API connection string

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 import logo from '../assets/logo.svg';
 
-function Login() {
+function Login({ history }) {
+
+    const [ username, setUsername ] = useState(''); // inicializada com valor vazio
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(username);
+
+        history.push('/main');
+    }
+
     return (
         <div className="login-container">
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <img src={logo} alt="Tindev logo" />
                 <input 
                     placeholder="Digite seu usuario no Github"
+                    value={username}
+                    onChange={ e => setUsername(e.target.value) }
                 />
                 <button type="submit">Enviar</button>
             </form>
